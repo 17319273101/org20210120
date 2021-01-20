fb58f888deef34a08f590ab894b1a9a73caa1ba1
xiaoguang <953765220@qq.com>
2021-01-20 20:14:37 +0800

    修改lodash.js

--- lodash.js
+++ lodash.js
@@ -7,3 +7,6 @@ console.log(_.chunk(['a', 'b', 'c', 'd'], 3));
 //获取一个随机数
 console.log(_.random(1,100));
 
+// 时间算法模块
+const moment=require("moment")
+console.log(moment("19921216", "YYYYMMDD").fromNow())
